b2b6562136e9665ac4cf43fa22183d88d07806cf
Thepphithak <95979163+E3tow1C@users.noreply.github.com>
2025-02-04 04:58:28 +0700

    Add image attachment support and validations to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+  has_many_attached :images
+
+  validates :topic, presence: true, length: { maximum: 150 }
+  validates :body, presence: true, length: { maximum: 5000 }
+  validates :images, presence: true
 end
