a5c09ddad12cd1457d12d3af37ced8644805b083
Thepphithak <95979163+E3tow1C@users.noreply.github.com>
2025-02-04 17:59:53 +0700

    Update validations for article topic and body to require minimum length

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   has_many_attached :images
 
-  validates :topic, presence: true, length: { maximum: 150 }
-  validates :body, presence: true, length: { maximum: 5000 }
+  validates :topic, presence: true, length: { minimum: 1, maximum: 150 }
+  validates :body, presence: true, length: { minimum: 1, maximum: 5000 }
   validates :images, presence: true
 end
